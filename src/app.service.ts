ee8d9b783d6866b266c5cf530cae198d685940b1
marco <g.marcotaba@gmail.com>
2021-07-20 00:12:18 +0200

    modified

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,13 +6,17 @@ export class AppService {
     public async constructEventFromPayload(signature: string, payload: Buffer) {
       const webhookSecret = this.configService.get('WEBHOOK_SECRET');
       return this.webhooks.constructEvent(
+        //testing
+        //console.log(this.webhooks)
+        //console.log(webhook)
         payload,
         signature,
         webhookSecret
       )
+
     }
 
-    
+
   getHello(): string {
     return 'Hello World!';
   }
